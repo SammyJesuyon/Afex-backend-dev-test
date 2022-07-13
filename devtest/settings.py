f8d7e18c0384c38ea8b02738c8922e68d515e82e
SammyJesuyon <sammyjesuyon@yahoo.com>
2022-07-14 00:28:56 +0100

    update

--- devtest/settings.py
+++ devtest/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY", default=get_random_secret_key(), cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://pure-sands-29572.herokuapp.com']
 
 
 # Application definition
